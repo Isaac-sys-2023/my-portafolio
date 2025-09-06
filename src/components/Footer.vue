a6f58e7e1ac0d1c82a8570b046fb7a3a17eaa1c4
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-09-06 18:49:33 -0400

    feat: Create header and footer initial components

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import linkedin from './icons/linkedIn/linkedin.vue';
+import github from './icons/github-mark/github.vue';
+</script>
+
+<template>
+  <footer>
+    <github />
+    <linkedin />
+  </footer>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import DarkModeToggle from './DarkModeToggle.vue';
+
+
+</script>
+
+<template>
+  <header>
+    <ul class="nav-links">
+      <li>
+        <RouterLink to="/">Home</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/about">About</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/projects">Projects</RouterLink>
+      </li>
+    </ul>
+
+    <DarkModeToggle />
+  </header>
+</template>
+
+<style scoped>
+.nav-links {
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+  padding: 0;
+  margin: 0;
+}
+</style>
