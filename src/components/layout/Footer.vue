7e60ac7333416632301714af83d546d5e1ceca66
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-09-07 17:39:02 -0400

    refactor: move some files in specific folders

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import linkedin from '../icons/linkedIn/linkedin.vue';
+import github from '../icons/github-mark/github.vue';
+import email from '../icons/email/email.vue';
+</script>
+
+<template>
+  <footer>
+    <a href="">
+      <github />
+    </a>
+    <a href="">
+      <linkedin />
+    </a>
+    <a href="">
+      <email />
+    </a>
+  </footer>
+</template>
+
+<style scoped></style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import DarkModeToggle from '../toggle/DarkModeToggle.vue';
+
+
+</script>
+
+<template>
+  <header>
+    <ul class="nav-links">
+      <li>
+        <RouterLink to="/">Home</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/about">About</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/projects">Projects</RouterLink>
+      </li>
+    </ul>
+
+    <DarkModeToggle />
+  </header>
+</template>
+
+<style scoped>
+.nav-links {
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  padding: 0;
+  margin: 0;
+}
+</style>
